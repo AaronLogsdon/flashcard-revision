be6183a0384a69807edc80d979afdd993f44298a
boys <boys@Johns-MacBook-Air-2.local>
2018-11-29 11:37:08 +0000

    changed firebase.js

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,5 +1,6 @@
 import firebase from 'firebase/app';
-
+import 'firebase/auth';
+import 'firebase/database';
 const config = {
     apiKey: "AIzaSyAMzMQ7d9R3VySOCMBqvUncIPtMCQjPWpg",
     authDomain: "react-firebase-authentic-99189.firebaseapp.com",
