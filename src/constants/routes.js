cb11fa8425f23494fc2ac6e49cb5aceb7246f5ba
boys <boys@Johns-MacBook-Air-2.local>
2018-11-30 16:48:00 +0000

    adding changes

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -1,2 +1,10 @@
 export const SIGN_IN = '/signin';
+export const DASHBOARD = '/dashboard';
 export const HOME = '/home';
+export const TEST = '/test';
+export const STUDY = '/study';
+export const GAME1 = '/game1';
+export const GAME2 = '/game2';
+export const DECK = '/deck';
+export const VIEW = '/view';
+
