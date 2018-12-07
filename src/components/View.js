b13b6192ce51a8dadedd39216e0c73bd9508510b
boys <boys@Johns-MacBook-Air-2.local>
2018-12-07 10:27:19 +0000

    half way stage of adddecks

--- src/components/View.js
+++ src/components/View.js
@@ -3,22 +3,13 @@ import { Link, withRouter, } from 'react-router-dom';
 import * as routes from '../constants/routes';
 
 
-class View extends Component {
-  
-  render() {
-    return (
-      <div>
-        <h1>View</h1>
-      </div>
-    );
-  }
-}
-
 const ViewLink = () =>
   <p>
     <Link to={routes.VIEW}>View</Link>
   </p>
 
-export default withRouter(View);
+
 
 export {ViewLink};
+
+
