90374bc2f3bb03d90ee77c6cb3b8634e11b1b31c
boys <boys@Johns-MacBook-Air-2.local>
2018-12-07 13:28:45 +0000

    edit

--- src/firebase/db.js
+++ src/firebase/db.js
@@ -1,14 +1,5 @@
 import { db } from './firebase';
 
-// User API
-
-// export const doCreateUser = (id, username, email) =>
-//   db.ref(`users/${id}`).set({
-//     username,
-//     email,
-//   });
-
 export const onceGetUsers = () =>
   db.ref('users').once('value');
 
-// Other Entity APIs ...
